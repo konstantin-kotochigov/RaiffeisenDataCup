83a81e0de7f622e0d890323b073f5327757540f9
Konstantin Kotochigov <k.kotochigov@cleverdata.ru>
2018-03-27 17:00:54 +0300

    changed file names

--- app.R
+++ app.R
@@ -2,25 +2,31 @@
 if (Sys.info()['sysname']=="Windows") setwd("C:/Users/konstantin/git/raiff/")
 
 # Load ML Utils
-source("code/0_utils.R")
+source("code/utils.R")
 
 # Load data
-source("code/1_load.R")
+source("code/load.R")
 
 # Filter data
-source("code/2_filter.R")
+source("code/dq.R")
 
 # Agregate data
-source("code/3_generate_pos_attributes.R")
+source("code/generate_pos_attributes.R")
 
 # Compute distance based attributes
-source("code/4_process_customer.R")
+source("code/process_customer_function.R")
 
 # Compute distance-based attributes
-source("code/5_generate_distance_attributes.R")
+source("code/generate_distance_attributes.R")
 
 # Feature Selection and Datasets
 source("code/feature_selection.R")
 
+# Optimize hyperparams
+source("code/model_optimize.R")
+
 # Fit models
 source("code/model.R")
+
+# Average Predictions
+# source("code/average.R")
